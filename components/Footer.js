52e9ebaf3383e06886db456309ef40ff83ea74c3
Timothy Rich <TimothyR2@reutech.co.za>
2021-10-13 14:30:57 +0200

    Fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -18,23 +18,23 @@ function Footer() {
                 <p>This is not a real site</p>
                 <p>Its a pretty awesome clone</p>
                 <p>Available to freelance</p>
-                <p>Trich</p>
+                <p className="text-blue-700 hover:scale-105 hover:text-bold"><a href="https://trich.bss.design">trich</a></p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">HOST</h5>
-                <p>How Airbnb works</p>
-                <p>News room</p>
-                <p>Investors</p>
-                <p>Airbnb Plus</p>
-                <p>Airbnb Luxe</p>
+                <p>Hosting</p>
+                <p>Super host</p>
+                <p>Middle host</p>
+                <p>Nomal host</p>
+                <p>Little host</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
                 <h5 className="font-bold">SUPOORT</h5>
-                <p>How Airbnb works</p>
-                <p>News room</p>
-                <p>Investors</p>
-                <p>Airbnb Plus</p>
-                <p>Airbnb Luxe</p>
+                <p>Get support</p>
+                <p>Lost keys</p>
+                <p>Missing room</p>
+                <p>Low oxygen support</p>
+                <p>Brick foundation</p>
             </div>
         </div>
     )
