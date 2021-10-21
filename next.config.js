2278f8ef8c1eda2ba5d039814aa5346ac46ca1e0
Timothy Rich <3647567@myuwc.ac.za>
2021-10-21 14:13:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     images: {
         domains: ["links.papareact.com"]
     },
-    env: {
-        mapbox_key: 'pk.eyJ1IjoidC1yaWNoIiwiYSI6ImNrdXo3dTM0dTJ2eWMybnF2ZWZ3dTh3YmsifQ.SfvJCVReXsU17wIA3ZDXlg'
-    }
+//     env: {
+//         mapbox_key: 'pk.eyJ1IjoidC1yaWNoIiwiYSI6ImNrdXo3dTM0dTJ2eWMybnF2ZWZ3dTh3YmsifQ.SfvJCVReXsU17wIA3ZDXlg'
+//     }
 };
